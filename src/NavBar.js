3baf179a4e943869b100b20a3c85f4ba1884c78f
github username <github email address>
2018-01-19 15:16:05 -0500

    made navbar links into items

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,9 +6,9 @@ class NavBar extends Component {
   render() {
     return (
       <div class="ui top menu">
-        <NavLink to="/" exact> Home </NavLink>
-        <NavLink to="/soccer" exact> Soccer </NavLink>
-        <NavLink to="/teams" exact> Teams </NavLink>
+        <NavLink to="/" exact className="item"> Home </NavLink>
+        <NavLink to="/soccer" exact className="item"> Soccer </NavLink>
+        <NavLink to="/teams" exact className="item"> Teams </NavLink>
       </div>
     )
   }
