a7577801b56b26d3cd89bdb5048beb2e38ade296
erickalexander <erickcamacho1@gmail.com>
2018-01-23 17:46:09 -0500

    created table

--- src/LeagueShow.js
+++ src/LeagueShow.js
@@ -7,12 +7,60 @@ const imgStyle = {
 
 const LeagueShow = ({league}) => {
 
-  console.log(league);
+  console.log("Currently in league show", league);
   return (
-    <div>
+    <div className="ui grid">
+      <div className="eight wide column">
         <h1>{league.name}</h1>
-
         <img style={imgStyle} src={league.img}/>
+      </div>
+
+      <div className="eight wide column">
+        <h1 className="ui center aligned header">League Table</h1>
+        <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">
+              Team Name
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Position
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Points
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Wins
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Losses
+            </h3>
+          </th>
+        </tr>
+        {league.teams.map(team => {
+          return (
+            <tr>
+              <td>{team.name}</td>
+              <td>{team.position}</td>
+              <td>{team.points}</td>
+              <td>{team.wins}</td>
+              <td>{team.losses}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+
+      </div>
     </div>
   )
 
