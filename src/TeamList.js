04bbcaf994c8fe0c403d2659c35c16a2da242836
Josh Wasserman <github email address>
2018-04-02 18:24:42 -0400

    Fixed layout

--- src/TeamList.js
+++ src/TeamList.js
@@ -1,74 +1,86 @@
-import React from 'react'
-import TeamCard from './TeamCard'
-import TeamShow from './TeamShow'
-import Adapter from './Adapter'
-import SearchTeams from "./SearchTeams"
-import {  Route,  Switch,  } from 'react-router-dom';
+import React from "react";
+import TeamCard from "./TeamCard";
+import TeamShow from "./TeamShow";
+import Adapter from "./Adapter";
+import SearchTeams from "./SearchTeams";
+import { Route, Switch } from "react-router-dom";
 
+class TeamList extends React.Component {
+  constructor(props) {
+    super(props);
 
-
-class TeamList extends React.Component{
-  constructor(props){
-    super(props)
-
-    this.state={
+    this.state = {
       teams: [],
-      search: "",
-    }
+      search: ""
+    };
   }
 
-  componentDidMount(){
-    Adapter.getTeams()
-    .then(teams => this.setState({
-      teams: teams
-    }))
-
-
+  componentDidMount() {
+    Adapter.getTeams().then(teams =>
+      this.setState({
+        teams: teams
+      })
+    );
   }
 
-  handleSearchChange = (event) =>{
+  handleSearchChange = event => {
     console.log(event.target.value);
     this.setState({
       search: event.target.value
-    })
-  }
+    });
+  };
 
-
-
-
-
-  render(){
+  render() {
     console.log("state", this.state.teams);
-    return(
+    return (
       <div className="column">
         <Switch>
-            <Route path='/teams/:id' render = {({match}) => {
+          <Route
+            path="/teams/:id"
+            render={({ match }) => {
               console.log(match.params);
-              const team = this.state.teams.find(t=> t.id === parseInt(match.params.id,10))
+              const team = this.state.teams.find(
+                t => t.id === parseInt(match.params.id, 10)
+              );
               console.log("/teams Team", team);
-              return team ? <TeamShow team={team}/> : <div>Loading...</div>
-            }}/>
-            <Route exact path='/teams' render = { () => {
-                console.log("exact path /teams");
-                return (
-                  <div>
+              return team ? <TeamShow team={team} /> : <div>Loading...</div>;
+            }}
+          />
+          <Route
+            exact
+            path="/teams"
+            render={() => {
+              console.log("exact path /teams");
+              return (
+                <div>
+                  <SearchTeams
+                    search={this.state.search}
+                    handleChange={this.handleSearchChange}
+                  />
 
-                      <SearchTeams search={this.state.search} handleChange={this.handleSearchChange}/>
-
-                    <div className="ui stackable center aligned page grid">
-                    {this.state.teams.filter(t => t.name.toLowerCase().includes(this.state.search.toLowerCase()))
-                      .map((team,index) => <TeamCard currentUser={this.props.currentUser} team={team} key={index}/>)}
-                    </div>
+                  <div className="ui stackable center aligned  grid">
+                    {this.state.teams
+                      .filter(t =>
+                        t.name
+                          .toLowerCase()
+                          .includes(this.state.search.toLowerCase())
+                      )
+                      .map((team, index) => (
+                        <TeamCard
+                          currentUser={this.props.currentUser}
+                          team={team}
+                          key={index}
+                        />
+                      ))}
                   </div>
-                )}
-              }/>
-
+                </div>
+              );
+            }}
+          />
         </Switch>
-
       </div>
-    )
+    );
   }
-
 }
 // const TeamList = ({teams}) => {
 //   const handleCLick = (team) =>{
@@ -92,5 +104,4 @@ class TeamList extends React.Component{
 //   )
 // }
 
-
-export default TeamList
+export default TeamList;
